121307dcb74b1c3f02a8ff63946e9417f7722cff
jomazing <jomarribu@gmail.com>
2015-10-28 14:21:38 -0700

    Initial Commit

---
+++ src/uno/Uno.java
@@ -0,0 +1,250 @@
+
+package uno;
+
+import java.io.*;
+import javax.sound.sampled.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+
+public class Uno extends JFrame implements Runnable {
+    static final int WINDOW_WIDTH = 1280;
+    static final int WINDOW_HEIGHT = 760;
+    final int XBORDER = 20;
+    final int YBORDER = 20;
+    final int YTITLE = 25;
+    boolean animateFirstTime = true;
+    int xsize = -1;
+    int ysize = -1;
+    Image image;
+    Graphics2D g;
+
+    sound zsound = null;
+    sound bgSound = null;
+
+//variables for rocket.
+    
+
+    static Uno frame;
+    public static void main(String[] args) {
+        frame = new Uno();
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public Uno() {
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (e.BUTTON1 == e.getButton()) {
+                    //left button
+
+// location of the cursor.
+                    int xpos = e.getX();
+                    int ypos = e.getY();
+
+                }
+                if (e.BUTTON3 == e.getButton()) {
+                    //right button
+                    reset();
+                }
+                repaint();
+            }
+        });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent e) {
+        repaint();
+      }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseMoved(MouseEvent e) {
+
+        repaint();
+      }
+    });
+
+        addKeyListener(new KeyAdapter() {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.VK_UP == e.getKeyCode()) {
+                } 
+                else if (e.VK_DOWN == e.getKeyCode()) {
+                } 
+                else if (e.VK_LEFT == e.getKeyCode()) {
+
+                } 
+                else if (e.VK_RIGHT == e.getKeyCode()) {
+  
+                }
+                else if (e.VK_INSERT == e.getKeyCode()) {                    
+                }
+                if (e.VK_SPACE == e.getKeyCode())
+                {
+                    
+                }
+                
+                repaint();
+            }
+        });
+        init();
+        start();
+    }
+    Thread relaxer;
+////////////////////////////////////////////////////////////////////////////
+    public void init() {
+        requestFocus();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void destroy() {
+    }
+
+
+
+////////////////////////////////////////////////////////////////////////////
+    public void paint(Graphics gOld) {
+        if (image == null || xsize != getSize().width || ysize != getSize().height) {
+            xsize = getSize().width;
+            ysize = getSize().height;
+            image = createImage(xsize, ysize);
+            g = (Graphics2D) image.getGraphics();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+//fill background
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, xsize, ysize);
+
+        int x[] = {getX(0), getX(getWidth2()), getX(getWidth2()), getX(0), getX(0)};
+        int y[] = {getY(0), getY(0), getY(getHeight2()), getY(getHeight2()), getY(0)};
+//fill border
+        g.setColor(Color.white);
+        g.fillPolygon(x, y, 4);
+// draw border
+        g.setColor(Color.red);
+        g.drawPolyline(x, y, 5);
+
+        if (animateFirstTime) {
+            gOld.drawImage(image, 0, 0, null);
+            return;
+        }
+
+        
+        
+        gOld.drawImage(image, 0, 0, null);
+    }
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+// needed for     implement runnable
+    public void run() {
+        while (true) {
+            animate();
+            repaint();
+            double seconds = 0.04;    //time that 1 frame takes.
+            int miliseconds = (int) (1000.0 * seconds);
+            try {
+                Thread.sleep(miliseconds);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void reset() {
+
+//init the location of the rocket to the center.
+        
+
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void animate() {
+        if (animateFirstTime) {
+            animateFirstTime = false;
+            if (xsize != getSize().width || ysize != getSize().height) {
+                xsize = getSize().width;
+                ysize = getSize().height;
+            }
+            
+
+    }
+    }
+
+////////////////////////////////////////////////////////////////////////////
+    public void start() {
+        if (relaxer == null) {
+            relaxer = new Thread(this);
+            relaxer.start();
+        }
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void stop() {
+        if (relaxer.isAlive()) {
+            relaxer.stop();
+        }
+        relaxer = null;
+    }
+/////////////////////////////////////////////////////////////////////////
+    public int getX(int x) {
+        return (x + XBORDER);
+    }
+
+    public int getY(int y) {
+        return (y + YBORDER + YTITLE);
+    }
+
+    public int getYNormal(int y) {
+        return (-y + YBORDER + YTITLE + getHeight2());
+    }
+    
+    
+    public int getWidth2() {
+        return (xsize - getX(0) - XBORDER);
+    }
+
+    public int getHeight2() {
+        return (ysize - getY(0) - YBORDER);
+    }
+}
+
+class sound implements Runnable {
+    Thread myThread;
+    File soundFile;
+    public boolean donePlaying = false;
+    sound(String _name)
+    {
+        soundFile = new File(_name);
+        myThread = new Thread(this);
+        myThread.start();
+    }
+    public void run()
+    {
+        try {
+        AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+        AudioFormat format = ais.getFormat();
+    //    System.out.println("Format: " + format);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+        SourceDataLine source = (SourceDataLine) AudioSystem.getLine(info);
+        source.open(format);
+        source.start();
+        int read = 0;
+        byte[] audioData = new byte[16384];
+        while (read > -1){
+            read = ais.read(audioData,0,audioData.length);
+            if (read >= 0) {
+                source.write(audioData,0,read);
+            }
+        }
+        donePlaying = true;
+
+        source.drain();
+        source.close();
+        }
+        catch (Exception exc) {
+            System.out.println("error: " + exc.getMessage());
+            exc.printStackTrace();
+        }
+    }
+
+}
