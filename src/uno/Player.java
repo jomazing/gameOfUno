9f43e166360e8dde12f7f058c1661e2b23044385
jomazing <jomarribu@gmail.com>
2015-10-29 13:34:53 -0700

    addedTurn

--- src/uno/Player.java
+++ src/uno/Player.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uno;
 
-/**
- *
- * @author 152003660
- */
 public class Player {
+    
+    private boolean turn;
+    
     
 }
